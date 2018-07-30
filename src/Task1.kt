cd302bdacba66f6641edc881f79eaa1ee00edb22
DESKTOP-MQ2QD0K\Y520 <bogdan.halmaghi@gmail.com>
2018-07-30 23:04:06 +0300

    Refactor first task

--- src/Task1.kt
+++
@@ -1,3 +0,0 @@
-package test.agileFreaks
-
-fun IntArray.Task1():Int = this.filter {it % 2 == 0}.sum()
---
+++ src/Tasks.kt
@@ -0,0 +1,10 @@
+package test.agileFreaks
+
+fun IntArray.sumOfEven():Int {
+    var sum =0
+    this.forEach {
+        if (it % 2 == 0) sum += it
+    }
+    return sum;
+}
+
