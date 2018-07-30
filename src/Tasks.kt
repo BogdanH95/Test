7c97b3250ccbabe14e4d7b2ce0177ff814f89b66
DESKTOP-MQ2QD0K\Y520 <bogdan.halmaghi@gmail.com>
2018-07-31 00:23:45 +0300

    Implement third task

--- src/Tasks.kt
+++ src/Tasks.kt
@@ -24,5 +24,16 @@ fun IntArray.firstTwoLargestIndexes():IntArray {
 
 //Made it function in order to manipulate the array
 fun hasDuplicates(array : IntArray): Boolean {
+    if(array.count() < 2) return false
+
+    array.forEach{
+        val indexNormalized = Math.abs(it) -1
+        if (array.elementAt(indexNormalized) < 0)
+            array.set(
+                    index = indexNormalized,
+                    value = -array.elementAt(indexNormalized)
+            )
+        else return true
+    }
     return false
 }
