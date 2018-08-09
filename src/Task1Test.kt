6bf9daca27e12e536d04fc5ada88940cdd7f0dba
DESKTOP-MQ2QD0K\Y520 <bogdan.halmaghi@gmail.com>
2018-08-09 20:20:08 +0300

    Added missing test cases for task 1

--- src/Task1Test.kt
+++ src/Task1Test.kt
@@ -22,4 +22,14 @@ internal class Task1Test {
     fun sumOfEvenOnArrayOfInt(){
         assertEquals(6, intArrayOf(1,1,2,1,2,1,1,2,1,1).sumOfEven())
     }
+
+    @Test
+    fun sumOfEvenOnArrayOfEvenInt(){
+        assertEquals(6, intArrayOf(2,0,0,-2,0,4,2,6,-6,-2).sumOfEven())
+    }
+
+    @Test
+    fun sumOfEvenOnArrayOfOddInt(){
+        assertEquals(0, intArrayOf(1,3,5,7,9,11).sumOfEven())
+    }
 }
