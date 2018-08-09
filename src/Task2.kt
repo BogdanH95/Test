ca8b8d39358a79f79ee4b5a024428d6898788481
DESKTOP-MQ2QD0K\Y520 <bogdan.halmaghi@gmail.com>
2018-08-09 20:41:25 +0300

    Refactored second task

--- src/Task2.kt
+++ src/Task2.kt
@@ -1,7 +1,5 @@
 
 fun IntArray.firstTwoLargestIndexes():IntArray {
-    if(this.count() < 2 ) return intArrayOf( Int.MIN_VALUE, Int.MIN_VALUE)
-
     var max = Int.MIN_VALUE
     var theOtherOne = Int.MIN_VALUE
 
