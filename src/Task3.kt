5d85356f6520c418d35a54c1c934eb3c6f642d33
DESKTOP-MQ2QD0K\Y520 <bogdan.halmaghi@gmail.com>
2018-08-09 22:38:20 +0300

    Bug fixed

--- src/Task3.kt
+++ src/Task3.kt
@@ -5,7 +5,7 @@ fun hasDuplicates(array : IntArray): Boolean {
 
     array.forEach{
         val indexNormalized = Math.abs(it) -1
-        if (array.elementAt(indexNormalized) < 0)
+        if (array.elementAt(indexNormalized) > 0)
             array.set(
                     index = indexNormalized,
                     value = -array.elementAt(indexNormalized)
