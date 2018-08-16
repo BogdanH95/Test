64c5c2e0148e7c3cada40a7f061c32a68399cd3b
DESKTOP-MQ2QD0K\Y520 <bogdan.halmaghi@gmail.com>
2018-08-16 21:03:40 +0300

    Task 1 made as a one liner

--- src/Task1.kt
+++ src/Task1.kt
@@ -1,11 +1,5 @@
 
-fun IntArray.sumOfEven():Int {
-    var sum = 0
-    this.forEach{
-    if (it.isEven()) sum += it
-    }
-    return sum
-}
+fun IntArray.sumOfEven ():Int = this.filter {it.isEven()}.sum()
 
 fun Int.isEven(): Boolean =  this % 2 == 0
 
