6740b8cb17a7b72cc76876851188dfe718a0cca1
DESKTOP-MQ2QD0K\Y520 <bogdan.halmaghi@gmail.com>
2018-08-16 21:34:23 +0300

    Removed unnecessary check

--- src/Task3.kt
+++ src/Task3.kt
@@ -1,10 +1,7 @@
 
-//Made it function in order to manipulate the array
-fun hasDuplicates(array : IntArray): Boolean {
-    if(array.count() < 2) return false
-
-    array.forEach{
-        val indexNormalized = Math.abs(it) -1
+fun hasDuplicates(array: IntArray): Boolean {
+    array.forEach {
+        val indexNormalized = Math.abs(it) - 1
         if (array.elementAt(indexNormalized) > 0)
             array.set(
                     index = indexNormalized,
